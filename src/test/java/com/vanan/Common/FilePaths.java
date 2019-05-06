08b4aa22951685b6ff161dbb55f96ea0727e05a5
manikavasagam-vananservices <32132366+manikavasagam-vananservices@users.noreply.github.com>
2019-05-06 21:29:44 +0530

    file separator modified

--- src/test/java/com/vanan/Common/FilePaths.java
+++ src/test/java/com/vanan/Common/FilePaths.java
@@ -1,7 +1,7 @@
 package com.vanan.Common;
 
 public interface FilePaths {
-    public String screenshotParentPath = System.getProperty("user.dir") + "\\src\\screenshots\\";
+    public String screenshotParentPath = System.getProperty("user.dir") +  "//screenshots//";
     public String testDataParentPath = System.getProperty("user.dir") + "//src//test//resources//testdata//";
     // Basic price calculation(Transcription, Translation, Captioning, Typing)
     public String level1 = testDataParentPath + "EasyQuoteL1.xlsx";
